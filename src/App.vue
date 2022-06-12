57a37167092cd287d5ce11a4a9f883ace251f471
microHoffman <skozak@protonmail.com>
2022-06-12 15:54:41 +0200

    added loaders to the app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <input class="input" v-model="address" @keyup.enter="analyzeContract" />
   <button class="button" @click="analyzeContract">Analyze contract</button>
-  <div v-if="contractDeployTime">
-    contract deploy time: {{ contractDeployTime }}
+  <div class="contract-deploy-time">
+    contract deploy time:
+    <div v-if="isLoadingContractDeployTime" class="loader"></div>
+    <template v-else-if="contractDeployTime">{{ contractDeployTime }}</template>
   </div>
-  <div v-if="nativeTransactions">
-    amount of transactions: {{ nativeTransactions.total }}
+  <div>
+    amount of transactions:
+    <div v-if="isLoadingNativeTransactions" class="loader"></div>
+    <template v-else-if="nativeTransactions?.total || nativeTransactions?.total === 0">{{ nativeTransactions.total }}</template>
   </div>
 </template>
 
@@ -18,13 +22,52 @@ const address = ref("")
 const nativeTransactions = ref()
 const contractDeployTime = ref()
 
+const isLoadingContractDeployTime = ref(false)
+const isLoadingNativeTransactions = ref(false)
+
 const analyzeContract = async (): Promise<void> => {
-  getContractDeployTime(address.value).then(deployTime => (contractDeployTime.value = deployTime))
-  getContractNativeTransactions(address.value).then(contractNativeTransactions => (nativeTransactions.value = contractNativeTransactions))
+  [isLoadingNativeTransactions.value, isLoadingContractDeployTime.value] = [true, true]
+  getContractNativeTransactions(address.value).then(contractNativeTransactions => {
+    nativeTransactions.value = contractNativeTransactions
+    isLoadingNativeTransactions.value = false
+  })
+  getContractDeployTime(address.value).then(deployTime => {
+    contractDeployTime.value = deployTime
+    isLoadingContractDeployTime.value = false
+  })
 }
 </script>
 
 
 <style>
+.loader {
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+}
 
+.loader:after {
+  content: " ";
+  display: block;
+  width: 16px;
+  height: 16px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 4px solid black;
+  border-color: black transparent black transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.contract-deploy-time {
+  margin-bottom: 0.5rem;
+}
 </style>
